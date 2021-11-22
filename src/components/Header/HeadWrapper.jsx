9f8780f2871909a92a2c17839ea2b3b993294aa7
John Phillips <johnphillips.dev@gmail.com>
2021-11-22 11:03:03 +0000

    Mobile menu toggle state complete.

--- src/components/Header/HeadWrapper.jsx
+++ src/components/Header/HeadWrapper.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Logo from '../../images/logo.svg';
 import Hamburger from '../../images/icon-hamburger.svg';
@@ -6,15 +6,26 @@ import Hamburger from '../../images/icon-hamburger.svg';
 import Navigation from './Navigation';
 
 function HeadWrapper() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <header className="bg-hero-pattern-mobile bg-center bg-cover lg:bg-hero-pattern-desktop">
       <div className="w-10/12 max-w-7xl m-auto h-screen">
         <div className="py-10 flex items-center justify-between">
-          <img className="w-40" src={Logo} alt="" aria-hidden="true" />
-          <button className="lg:hidden">
+          <img
+            className={`w-40 ${isMenuOpen ? 'hidden lg:block' : ''}`}
+            src={Logo}
+            alt=""
+            aria-hidden="true"
+          />
+          <button
+            className={`${isMenuOpen ? 'hidden' : ''} lg:hidden`}
+            aria-expanded={isMenuOpen}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            onKeyPress={() => setIsMenuOpen(!isMenuOpen)}
+          >
             <img src={Hamburger} alt="" aria-hidden="true" />
           </button>
-          <Navigation />
+          <Navigation isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
         </div>
         <h1 className="text-white text-5xl p-5 mt-24 w-full max-w-sm uppercase font-josefin border-4 lg:text-7xl lg:border-2 lg:max-w-2xl">
           Immersive experiences that deliver
--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -3,12 +3,21 @@ import React from 'react';
 import Logo from '../../images/logo.svg';
 import CloseIcon from '../../images/icon-close.svg';
 
-function Navigation() {
+function Navigation({ isMenuOpen, setIsMenuOpen }) {
   return (
-    <nav className="w-full h-screen bg-black absolute top-0 left-0 lg:block lg:relative lg:bg-transparent lg:left-auto lg:bottom-auto lg:h-auto lg:w-auto">
-      <div className="flex p-8 lg:hidden">
+    <nav
+      className={`${
+        isMenuOpen ? '' : 'hidden'
+      } w-full h-screen bg-black absolute top-0 left-0 lg:block lg:relative lg:bg-transparent lg:left-auto lg:bottom-auto lg:h-auto lg:w-auto`}
+    >
+      <div className="flex p-8 py-10 lg:hidden">
         <img className="w-40 mr-auto" src={Logo} alt="" aria-hidden="true" />
-        <button className="lg:hidden">
+        <button
+          className="lg:hidden"
+          aria-expanded="true"
+          onClick={() => setIsMenuOpen(false)}
+          onKeyPress={() => setIsMenuOpen(false)}
+        >
           <img src={CloseIcon} alt="" aria-hidden="true" />
         </button>
       </div>
