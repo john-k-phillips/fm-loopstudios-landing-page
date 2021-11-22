c2c817a20609a60e5511dfb83f510fff8edc0dc3
John Phillips <johnphillips.dev@gmail.com>
2021-11-22 18:13:38 +0000

    Added background images for gallary.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,37 @@ module.exports = {
         'interactive-mobile': "url('/src/images/mobile/image-interactive.jpg')",
         'interactive-desktop':
           "url('/src/images/desktop/image-interactive.jpg')",
+
+        // Gallary Mobile Images
+        'curiosity-mobile': "url('/src/images/mobile/image-curiosity.jpg')",
+        'fisheye-mobile': "url('/src/images/mobile/image-fisheye.jpg')",
+        'soccer-team-mobile': "url('/src/images/mobile/image-soccer-team.jpg')",
+        'deep-earth-mobile': "url('/src/images/mobile/image-deep-earth.jpg')",
+        'grid-mobile': "url('/src/images/mobile/image-grid.jpg')",
+        'night-arcade-mobile':
+          "url('/src/images/mobile/image-night-arcade.jpg')",
+        'from-above-mobile': "url('/src/images/mobile/image-from-above.jpg')",
+        'pocket-borealis-mobile':
+          "url('/src/images/mobile/image-pocket-borealis.jpg')",
+
+        // Gallary Desktop Images
+        'curiosity-desktop': "url('/src/images/desktop/image-curiosity.jpg')",
+        'fisheye-desktop': "url('/src/images/desktop/image-fisheye.jpg')",
+        'soccer-team-desktop':
+          "url('/src/images/desktop/image-soccer-team.jpg')",
+        'deep-earth-desktop': "url('/src/images/desktop/image-deep-earth.jpg')",
+        'grid-desktop': "url('/src/images/desktop/image-grid.jpg')",
+        'night-arcade-desktop':
+          "url('/src/images/desktop/image-night-arcade.jpg')",
+        'from-above-desktop': "url('/src/images/desktop/image-from-above.jpg')",
+        'pocket-borealis-desktop':
+          "url('/src/images/desktop/image-pocket-borealis.jpg')",
       },
       fontSize: {
-        '5xl': '2.5rem',
+        '5xl': '2.8rem',
+      },
+      height: {
+        xxl: '500px',
       },
     },
   },
