eebbcc2f9e64b2ee86f63253af1e4f1b32104fca
John Phillips <johnphillips.dev@gmail.com>
2021-11-22 10:55:16 +0000

    Navigation styled for desktop

--- src/components/Header/HeadWrapper.jsx
+++ src/components/Header/HeadWrapper.jsx
@@ -3,34 +3,18 @@ import React from 'react';
 import Logo from '../../images/logo.svg';
 import Hamburger from '../../images/icon-hamburger.svg';
 
+import Navigation from './Navigation';
+
 function HeadWrapper() {
   return (
     <header className="bg-hero-pattern-mobile bg-center bg-cover lg:bg-hero-pattern-desktop">
-      <div className="w-10/12 max-w-7xl m-auto pb-60">
+      <div className="w-10/12 max-w-7xl m-auto h-screen">
         <div className="py-10 flex items-center justify-between">
           <img className="w-40" src={Logo} alt="" aria-hidden="true" />
           <button className="lg:hidden">
             <img src={Hamburger} alt="" aria-hidden="true" />
           </button>
-          <nav className="hidden lg:block">
-            <ul className="flex gap-5 text-white font-medium">
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#careers">Careers</a>
-              </li>
-              <li>
-                <a href="#events">Events</a>
-              </li>
-              <li>
-                <a href="#products"></a>Products
-              </li>
-              <li>
-                <a href="#support"></a>Support
-              </li>
-            </ul>
-          </nav>
+          <Navigation />
         </div>
         <h1 className="text-white text-5xl p-5 mt-24 w-full max-w-sm uppercase font-josefin border-4 lg:text-7xl lg:border-2 lg:max-w-2xl">
           Immersive experiences that deliver
---
+++ src/components/Header/Navigation.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Logo from '../../images/logo.svg';
+import CloseIcon from '../../images/icon-close.svg';
+
+function Navigation() {
+  return (
+    <nav className="w-full h-screen bg-black absolute top-0 left-0 lg:block lg:relative lg:bg-transparent lg:left-auto lg:bottom-auto lg:h-auto lg:w-auto">
+      <div className="flex p-8 lg:hidden">
+        <img className="w-40 mr-auto" src={Logo} alt="" aria-hidden="true" />
+        <button className="lg:hidden">
+          <img src={CloseIcon} alt="" aria-hidden="true" />
+        </button>
+      </div>
+
+      <ul className="flex flex-col mt-28 pl-8 font-josefin uppercase lg:capitalize text-4xl gap-5 lg:gap-7 text-white font-medium lg:flex-row lg:pl-0 lg:mt-0 lg:text-lg lg:font-alata">
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#careers">Careers</a>
+        </li>
+        <li>
+          <a href="#events">Events</a>
+        </li>
+        <li>
+          <a href="#products">Products</a>
+        </li>
+        <li>
+          <a href="#support">Support</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
