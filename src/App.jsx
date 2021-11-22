4b81b977b03e66352c7ba824efbc1cc0f7c001ba
John Phillips <johnphillips.dev@gmail.com>
2021-11-22 01:05:51 +0000

    Comment template structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 
-import MobileHeroImage from './images/mobile/image-interactive.jpg';
-
 function App() {
   return (
     <div>
-      <img src={MobileHeroImage} alt="" />
-      <div>
-        <h2>The leader in interactive VR</h2>
-        <p>
-          Founded in 2011, Loopstudios has been producing world-class virtual
-          reality projects for some of the best companies around the globe. Our
-          award-winning creations have transformed businesses through digital
-          experiences that bind to their brand.
-        </p>
-      </div>
+      {/* Header */}
+      {/* Logo */}
+      {/* Navigation */}
+      {/* Immersive Box */}
+
+      {/* Main Content */}
+      {/* Leader Image */}
+      {/* Leader Content */}
+
+      {/* Grid Section */}
+      {/* Heading & Btn */}
+      {/* Gallary */}
+
+      {/* Footer */}
+      {/* Logo */}
+      {/* Social Icons */}
+      {/* Navigation */}
+      {/* Copyright */}
     </div>
   );
 }
