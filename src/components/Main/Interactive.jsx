5b5b96231c9635adc600bcef04a5567525a406ef
John Phillips <johnphillips.dev@gmail.com>
2021-11-22 17:03:51 +0000

    Replaced div with section.

--- src/components/Main/Interactive.jsx
+++ src/components/Main/Interactive.jsx
@@ -6,9 +6,14 @@ function Interactive() {
   return (
     <div className="w-10/12 max-w-7xl m-auto lg:flex lg:justify-start lg:items-center lg:relative lg:mb-20">
       <div className="bg-interactive-mobile bg-no-repeat bg-cover mt-40 mb-10 lg:bg-interactive-desktop lg:mb-0">
-        <img src={InteractiveImage} alt="" className="invisible" />
+        <img
+          src={InteractiveImage}
+          alt=""
+          className="invisible"
+          aria-hidden="true"
+        />
       </div>
-      <div className="text-center bg-white lg:text-left max-w-2xl lg:absolute lg:p-16 lg:pl-24 lg:pr-0 lg:pb-0 lg:bottom-0 lg:right-0">
+      <section className="text-center bg-white lg:text-left max-w-2xl lg:absolute lg:p-16 lg:pl-24 lg:pr-0 lg:pb-0 lg:bottom-0 lg:right-0">
         <h2 className="font-josefin uppercase text-4xl lg:text-6xl">
           The leader in interactive VR
         </h2>
@@ -18,7 +23,7 @@ function Interactive() {
           award-winning creations have transformed businesses through digital
           experiences that bind to their brand.
         </p>
-      </div>
+      </section>
     </div>
   );
 }
