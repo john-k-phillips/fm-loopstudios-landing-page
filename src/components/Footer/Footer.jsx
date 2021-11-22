8f7ddf3bec7f16c947a4376768a3695a70ca8e4a
John Phillips <johnphillips.dev@gmail.com>
2021-11-22 20:03:58 +0000

    Footer accessible, flex replaced with grid.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,48 +9,57 @@ import TwitterIcon from '../../images/icon-twitter.svg';
 
 function Footer() {
   return (
-    <footer className="flex flex-col justify-center p-10 bg-black text-white text-center">
-      <img src={Logo} alt="" aria-hidden="true" />
-      <ul className="flex flex-col gap-4 text-xl my-10">
-        <li>
-          <a href="about">About</a>
-        </li>
-        <li>
-          <a href="careers">Careers</a>
-        </li>
-        <li>
-          <a href="events">Events</a>
-        </li>
-        <li>
-          <a href="products">Products</a>
-        </li>
-        <li>
-          <a href="support">Support</a>
-        </li>
-      </ul>
-      <ul className="flex justify-center gap-4 my-5">
-        <li>
-          <a href="facebook">
-            <img src={FacebookIcon} alt="" />
-          </a>
-        </li>
-        <li>
-          <a href="twitter">
-            <img src={TwitterIcon} alt="" />
-          </a>
-        </li>
-        <li>
-          <a href="pinterest">
-            <img src={PinterestIcon} alt="" />
-          </a>
-        </li>
-        <li>
-          <a href="instagram">
-            <img src={InstagramIcon} alt="" />
-          </a>
-        </li>
-      </ul>
-      <p>© 2021 Loopstudios. All rights reserved.</p>
+    <footer className="bg-black text-white text-center">
+      <div className="grid py-10 w-10/12 max-w-6xl m-auto lg:grid-cols-2">
+        <img
+          className="justify-self-center lg:col-span-1 lg:justify-self-start"
+          src={Logo}
+          alt=""
+          aria-hidden="true"
+        />
+        <ul className="nav-hover flex flex-col gap-4 text-xl my-10 lg:flex-row lg:my-0">
+          <li>
+            <a href="about">About</a>
+          </li>
+          <li>
+            <a href="careers">Careers</a>
+          </li>
+          <li>
+            <a href="events">Events</a>
+          </li>
+          <li>
+            <a href="products">Products</a>
+          </li>
+          <li>
+            <a href="support">Support</a>
+          </li>
+        </ul>
+        <ul className="nav-hover-social flex justify-center gap-4 my-5 lg:col-start-2 lg:row-start-1 lg:justify-end">
+          <li>
+            <a href="facebook">
+              <img src={FacebookIcon} alt="" aria-label="facebook" />
+            </a>
+          </li>
+          <li>
+            <a href="twitter" aria-label="twitter">
+              <img src={TwitterIcon} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="pinterest">
+              <img src={PinterestIcon} alt="" aria-label="pinterest" />
+            </a>
+          </li>
+          <li>
+            <a href="instagram">
+              <img src={InstagramIcon} alt="" aria-label="instagram" />
+            </a>
+          </li>
+        </ul>
+        <p className="lg:text-right">
+          © 2021 Loopstudios. All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 }
