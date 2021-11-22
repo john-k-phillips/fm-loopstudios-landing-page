94ca9b1696c83495ca2c9ebc39d87af3aa1577ff
John Phillips <johnphillips.dev@gmail.com>
2021-11-22 00:51:25 +0000

    Removed JIT features.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import MobileHeroImage from './images/mobile/image-interactive.jpg';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <img src={MobileHeroImage} alt="" />
+      <div>
+        <h2>The leader in interactive VR</h2>
+        <p>
+          Founded in 2011, Loopstudios has been producing world-class virtual
+          reality projects for some of the best companies around the globe. Our
+          award-winning creations have transformed businesses through digital
+          experiences that bind to their brand.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
