782d69d3811f6b2576ab96d199a1c1e3c8a5b726
John Phillips <johnphillips.dev@gmail.com>
2021-11-22 18:13:47 +0000

    Gallary Added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import HeadWrapper from './components/Header/HeadWrapper';
+import Gallary from './components/Main/Gallary';
 import Interactive from './components/Main/Interactive';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       {/* Leader Content */}
       <main>
         <Interactive />
+        <Gallary />
       </main>
 
       {/* Grid Section */}
